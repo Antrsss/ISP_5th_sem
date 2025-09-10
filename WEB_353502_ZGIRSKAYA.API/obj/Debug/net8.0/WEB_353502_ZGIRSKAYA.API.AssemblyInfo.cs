d6fcb21f3be9a2a97b2b9d3d1624cacdfafaffa1
Darya Zgirskaya <129535397+Antrsss@users.noreply.github.com>
2025-09-10 17:44:29 +0300

    Dropdown cocktail categories menu added

--- WEB_353502_ZGIRSKAYA.API/obj/Debug/net8.0/WEB_353502_ZGIRSKAYA.API.AssemblyInfo.cs
+++ WEB_353502_ZGIRSKAYA.API/obj/Debug/net8.0/WEB_353502_ZGIRSKAYA.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEB_353502_ZGIRSKAYA.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca1f802faa6d24218005c0581b2215d264f19f4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4827cbbf99ebee84707753211e385fb6d5fd4bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEB_353502_ZGIRSKAYA.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEB_353502_ZGIRSKAYA.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
